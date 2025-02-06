8ca86a8362caf2f2f955b591bf30bd8530d94ca2
@pavel.shoba <pavel.shoba@neo.gg>
2025-02-07 01:43:29 +0300

    add test to add item in cart and check data

--- src/test/java/tests/AddItemCartTest.java
+++ src/test/java/tests/AddItemCartTest.java
@@ -1,13 +1,9 @@
 package tests;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
